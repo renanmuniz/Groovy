ef9c9f75a1f850635ba33cf50611d5d95d97544c
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-07-26 17:52:44 -0300

    Exercicio 2

--- aula/src/aula/Exercicios.groovy
+++ aula/src/aula/Exercicios.groovy
@@ -1,5 +1,6 @@
 package aula
 
+import classes.Cliente
 import org.junit.Test
 
 class Exercicios {
@@ -15,9 +16,15 @@ class Exercicios {
 
         BigDecimal valor = 200.50
         println valor
+    }
 
-
-
+    @Test
+    void exercicio2() {
+        Cliente c = new Cliente()
+        c.setNome "Renan"
+        c.setData new Date()
+        println c.somar(10,10)
+        println c.getNome() + " - " + c.getData()
     }
 
 }
