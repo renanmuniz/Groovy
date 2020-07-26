33e2ddd885b884914db482b2495fb48d6520a590
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-07-26 17:29:26 -0300

    Exercicio 1

---
+++ aula/src/Exercicio1.groovy
@@ -0,0 +1,14 @@
+
+class Exercicio1 {
+    static main(args) {
+        println "Ola mundo groovy"
+
+        String nome = "Renan"
+        int idade = 30
+
+        String frase = "o $nome tem $idade anos."
+        println frase
+
+
+    }
+}
---
+++ aula/src/Exercicios.groovy
@@ -0,0 +1,13 @@
+
+import org.junit.Test;
+
+class Exercicios {
+
+    @Test
+    void exercicio1ponto2() {
+        int a = 10;
+        a.
+
+    }
+
+}
